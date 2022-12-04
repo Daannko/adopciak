36d7cbe4b5b7fad9d82cb1a6b77c9725db81f64c
Anita Białorucka <hihcia580@gmail.com>
2022-12-04 22:52:26 +0100

    Zmiana wygladu home_screen

--- lib/model/styles.dart
+++ lib/model/styles.dart
@@ -2,18 +2,20 @@ import 'package:flutter/material.dart';
 
 class CustomStyles {
   static EdgeInsets marigin20 = const EdgeInsets.fromLTRB(20, 20, 20, 0);
-  static EdgeInsets mariginAnimal = const EdgeInsets.fromLTRB(5, 10, 5, 0);
+  static EdgeInsets mariginAnimal = const EdgeInsets.fromLTRB(5, 0, 5, 15);
   static EdgeInsets marigin10 = const EdgeInsets.fromLTRB(10, 10, 10, 0);
   static EdgeInsets listViewPaddingName =
       const EdgeInsets.fromLTRB(0, 10, 0, 0);
   static EdgeInsets listViewPadding = const EdgeInsets.fromLTRB(20, 10, 20, 10);
+  static EdgeInsets marginAnimalPhoto = const EdgeInsets.fromLTRB(5, 5, 5, 5);
   static double fontListView = 15;
   static double fontListViewName = 30;
   static EdgeInsets mariginsAll20 = const EdgeInsets.all(20);
   static EdgeInsets paddingAll7 = const EdgeInsets.all(7);
   static EdgeInsets paddingAll10 = const EdgeInsets.all(10);
   static EdgeInsets paddingAll20 = const EdgeInsets.all(20);
-  static EdgeInsets paddingSymmetric = EdgeInsets.symmetric(horizontal: 24.0);
+  static EdgeInsets paddingSymmetric =
+      const EdgeInsets.symmetric(horizontal: 24.0);
   static double fontSize20 = 20;
   static double fontSize18 = 18;
   static double fontSize15 = 15;
@@ -26,4 +28,8 @@ class CustomStyles {
   static BorderRadius radius20 = BorderRadius.circular(20);
   static BorderRadius radius30 = BorderRadius.circular(30);
   static BorderRadius radiusAnimalScreen = BorderRadius.circular(5);
+  static BorderRadius radiusAdoptuj = const BorderRadius.only(
+      bottomLeft: Radius.circular(5), bottomRight: Radius.circular(5));
+  static BorderRadius radiusAnimalPhoto =
+      const BorderRadius.only(topLeft: Radius.circular(5));
 }
