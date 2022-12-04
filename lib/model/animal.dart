36261f8e2dea641fae6c1125369423628fcce18d
Eryk Dorosz <228726@edu.p.lodz.pl>
2022-12-04 20:35:00 +0100

    supportedBy init

--- lib/model/animal.dart
+++ lib/model/animal.dart
@@ -1,3 +1,5 @@
+import 'package:adopciak/model/supports.dart';
+
 class Animal {
   int age = 0;
   String breed = "";
@@ -10,7 +12,9 @@ class Animal {
   String owner = "";
   String ownerUId = "";
   List<dynamic> needs = [];
-  List<dynamic> supports = [];
+  List<Map<String, dynamic>> supports = [
+    {'Amount': 0, 'Periodicity': 0, 'UserUid': ""}
+  ];
 
   Animal(this.uId, this.age, this.breed, this.name, this.info, this.location,
       this.owner, this.ownerUId, this.type, this.imageName);
